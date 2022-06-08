6e1d6031f9df6dbec23a775802560b1bf3daf491
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-06-08 18:42:23 +0900

    Routeの登録

--- app/Routing/Route.php
+++ app/Routing/Route.php
@@ -65,15 +65,20 @@ class Route
         $vars = [];
 
         foreach ($this->createTokens($request) as $exploded_uri_pattern => $exploded_uri) {
+            // URIにパラメータが存在するなら
+            // パラメータ名とURIをセットにして配列に保存
             if (str_starts_with($exploded_uri_pattern, ':')) {
                 $vars[ltrim($exploded_uri_pattern, ':')] = $exploded_uri;
             }
         }
 
+        // Closureで指定されているならClosureを
+        // Containerで指定されているならContainerをInstance化
         $handler = is_string($this->handler)
             ? $this->container->get($this->handler)
             : $this->handler;
 
+        // requestとパラメータの情報を基にRoutingに対応した処理を実行
         return $handler($request, $vars);
     }
 
@@ -87,10 +92,14 @@ class Route
         $exploded_uri_patterns = explode('/', ltrim($this->pass, '/'));
         $exploded_uris         = explode('/', ltrim($request->getPathInfo(), '/'));
 
+        // requestされたuriとframeworkで指定したパスが同じなら
+        // tokenはなし
         if (count($exploded_uri_patterns) !== count($exploded_uris)) {
             return [];
         }
 
+        // requestされたuriとframeworkで指定したパスが異なるなら
+        // requestされたuriとframeworkで指定したパスをセットにして配列を返す
         return array_combine($exploded_uri_patterns, $exploded_uris);
     }
 }
